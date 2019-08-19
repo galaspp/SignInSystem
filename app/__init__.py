371d21bd628b27ecaac98a1b4c4212c0791f9ba1
Piotr Galas <galaspp@rose-hulman.edu>
2019-08-19 10:07:31 -0500

    Changed running port

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ UPLOAD_FOLDER = 'Manufacturing'
 
 app = Flask(__name__)
 app.config.from_object(Config)
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
+app.run(host='0.0.0.0', port=80, debug=False)
 
 from app import routes
