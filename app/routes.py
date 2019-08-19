e2c3ea07c458c6edc7fe9dfa2f0cb1cbeef4ffa9
Piotr Galas <galaspp@rose-hulman.edu>
2019-08-19 11:05:57 -0500

    Change url name

--- app/routes.py
+++ app/routes.py
@@ -217,5 +217,5 @@ def taskShowcase():
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000, debug=True)
 else:
-	app.config['SERVER_NAME'] = 'RoseGPE.com'
+	app.config['SERVER_NAME'] = 'rosegpe.com'
 	app.run(host='0.0.0.0', port=8000, debug=True)
